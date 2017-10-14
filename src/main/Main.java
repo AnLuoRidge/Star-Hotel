52a08f003ddc8db05f0dc2911c49e17f821336d3
Ding <anluoridge@gmail.com>
2017-10-15 10:50:14 +1100

    clean code

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-// import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
@@ -12,17 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-//        System.out.println("Test...\nType anything:");
-//        Scanner scanner = new Scanner(System.in);
-//        String testString = scanner.next();
-//        System.out.println(testString);
-//
-
         Parent root = FXMLLoader.load(getClass().getResource("/main/mainView.fxml"));
-
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-
         Scene scene = new Scene(root, 600, 600);
         primaryStage.setTitle("xxx");
         primaryStage.setScene(scene);
