2fc4cc304a083a23bd2150a34d3460bd4eb21189
Juliicheng <32784308+Juliicheng@users.noreply.github.com>
2017-10-16 18:13:13 +0800

    Add files via upload

--- src/customer/list/CustomerListViewController.java
+++ src/customer/list/CustomerListViewController.java
@@ -16,29 +16,45 @@ import java.util.ResourceBundle;
 public class CustomerListViewController implements Initializable {
 
     @FXML
-    private Button addButton;
+    private Button addCustomerButton;
+
+    @FXML
+    private Button backButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        addButton.setOnAction(
+        addCustomerButton.setOnAction(
+                  event -> {
+
+                            ObservableList<Window> windows = Window.getWindows();
+                            Stage stage = (Stage) windows.get(0);
+                            Parent root;
+                            try {
+                                root = FXMLLoader.load(getClass().getResource("/customer/info/CustomerInfoView.fxml"));
+                                Scene scene = new Scene(root);
+                                stage.setScene(scene);
+                            } catch (Exception nn) {
+
+
+                            }
+                }
+        );
+        backButton.setOnAction(
                 event -> {
 
                     ObservableList<Window> windows = Window.getWindows();
                     Stage stage = (Stage) windows.get(0);
-                    System.out.println(stage);
                     Parent root;
                     try {
-                        root = FXMLLoader.load(getClass().getResource("/customer/info/CustomerInfoView.fxml"));
+                        root = FXMLLoader.load(getClass().getResource("/main/mainView.fxml"));
                         Scene scene = new Scene(root);
                         stage.setScene(scene);
                     } catch (Exception nn) {
 
-                        System.out.println(nn);
+
                     }
-
-
-//                    System.out.println("Touch add"); // Lambda
                 }
         );
+
     }
 }
